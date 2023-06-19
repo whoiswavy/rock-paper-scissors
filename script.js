a41989f67003ca23c020c72e64e908324ac1aeb7
0xSam <smorahschool@gmail.com>
2023-06-19 14:48:54 +0100

    added function playRound

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 function getComputerChoice() {
-    const choice = ['rock', 'paper', 'scissors'];
+    const choice = ['Rock', 'Paper', 'Scissors'];
     const randomElement = choice[Math.floor(Math.random() * choice.length)]
     return randomElement
 }
 
 function playRound(playerSelection, computerSelection) {
-    // your code here!
+    // playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection == computerSelection){
+        return "You Drew!"
+    } else if (((playerSelection == 'Rock') && (computerSelection == 'Paper')) || ((playerSelection == 'Scissors') && (computerSelection == 'Rock')) || ((playerSelection == 'Paper') && (computerSelection == 'Scissors'))) {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`
+    } else if (((playerSelection == 'Paper') && (computerSelection == 'Rock')) || ((playerSelection == 'Rock') && (computerSelection == 'Scissors')) || ((playerSelection == 'Scissors') && (computerSelection == 'Paper'))) {
+        return `You Win! ${computerSelection} beats ${playerSelection}`
+    }
   }
    
-  const playerSelection = "rock";
+  const playerSelection = "Rock";
   const computerSelection = getComputerChoice();
   console.log(playRound(playerSelection, computerSelection));
