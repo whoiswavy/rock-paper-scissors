1b18f4df19ff86a96d618d414f15705b4cb79f7f
0xSam <smorahschool@gmail.com>
2023-06-26 12:22:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+let playerScore = 0
+let computerScore = 0
+let roundWinner = '';
+let gameWinner = '';
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
+  }
+      
 function getComputerChoice() {
     const choice = ['Rock', 'Paper', 'Scissors'];
     const randomElement = choice[Math.floor(Math.random() * choice.length)]
@@ -8,14 +17,41 @@ function playRound(playerSelection, computerSelection) {
     // playerSelection = playerSelection.toLowerCase();
 
     if (playerSelection == computerSelection){
+        roundWinner = 'tie'
         return "You Drew!"
-    } else if (((playerSelection == 'Rock') && (computerSelection == 'Paper')) || ((playerSelection == 'Scissors') && (computerSelection == 'Rock')) || ((playerSelection == 'Paper') && (computerSelection == 'Scissors'))) {
-        return `You Lose! ${computerSelection} beats ${playerSelection}`
-    } else if (((playerSelection == 'Paper') && (computerSelection == 'Rock')) || ((playerSelection == 'Rock') && (computerSelection == 'Scissors')) || ((playerSelection == 'Scissors') && (computerSelection == 'Paper'))) {
+    }
+    if (
+        ((playerSelection == 'Rock') && (computerSelection == 'Paper')) || ((playerSelection == 'Scissors') && (computerSelection == 'Rock')) || ((playerSelection == 'Paper') && (computerSelection == 'Scissors'))
+    ) {
+        playerScore++
+        roundWinner = 'player'
+        return `You Lost! ${computerSelection} beats ${playerSelection}`
+    } 
+    if (
+        ((playerSelection == 'Paper') && (computerSelection == 'Rock')) || ((playerSelection == 'Rock') && (computerSelection == 'Scissors')) || ((playerSelection == 'Scissors') && (computerSelection == 'Paper'))
+    ) {
+        computerScore++
+        roundWinner = 'computer'
         return `You Win! ${computerSelection} beats ${playerSelection}`
     }
   }
-   
-  const playerSelection = "Rock";
+
+  
+  const playerSelection = capitalizeFirstLetter(prompt('Please select rock, paper, or scissors: '));
   const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
+
+  function game() {
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(playRound(playerSelection, computerSelection));
+    if (playerScore === 3) {
+        return 'You won the game!';
+    }
+    if (computerScore === 3) {
+        return 'Computer won the game!';
+    }
+  }
+
+  console.log(game())
